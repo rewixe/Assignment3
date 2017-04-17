c42925dcb86fa3ae76473543be588ae54f2892da
tnolan98 <c15406532@mydit.ie>
2017-04-17 12:45:17 +0100

    Changes

--- Game/src/Game.java
+++ Game/src/Game.java
@@ -1,5 +1,8 @@
+import processing.core.PApplet;
 
-public class Game {
+public class Game extends PApplet 
+{
+	public static Game applet;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
