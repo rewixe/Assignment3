bcae502480904fef82a9908e2a88acd5c1791ae9
tnolan98 <c15406532@mydit.ie>
2017-04-17 12:57:38 +0100

    New

--- Game/src/Game.java
+++ Game/src/Game.java
@@ -1,8 +1,13 @@
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 
 public class Game extends PApplet 
 {
 	public static Game applet;
+	
+	static ArrayList<GameObject> gameObjects = new ArrayList<GameObject>();
+	static int mode = 0;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
