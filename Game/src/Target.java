b6b5cc18ad8c712a31fa4b9bd5b2d8bb59a59624
tnolan98 <c15406532@mydit.ie>
2017-04-20 17:02:29 +0100

    Target changes

--- Game/src/Target.java
+++ Game/src/Target.java
@@ -1,9 +1,21 @@
 import processing.core.PApplet;
+import processing.core.PShape;
+import processing.core.PVector;
 
 public class Target extends GameObject implements SpecialObj
 {
 	public static Target applet;
 	
 	PApplet parent;
+	
+	float theta;
+	PShape test;
+	
+	Target(Game p)
+	{
+		parent = p;
+		theta = 0;
+		forward = new PVector(parent.random(-1,1), parent.random(-1,1));
+	}
 
 }
