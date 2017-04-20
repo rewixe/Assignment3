1936d712f198c1a9ea5ba78ffd0886eb5a6381df
tnolan98 <c15406532@mydit.ie>
2017-04-20 16:08:24 +0100

    shot class

--- Game/src/Shot.java
+++ Game/src/Shot.java
@@ -1,4 +1,13 @@
+import processing.core.PApplet;
 
-public class Shot {
-
+class Shot extends GameObject
+{
+	PApplet parent;
+	float theta;
+	float size;
+	float speed = 300;
+	float timeToLive;
+	float alive;
+	static int type;
+	
 }
