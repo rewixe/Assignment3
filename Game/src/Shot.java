e117194b8e10e39bf631a70ab72d460d7ec95245
tnolan98 <c15406532@mydit.ie>
2017-04-20 16:11:09 +0100

    shot class

--- Game/src/Shot.java
+++ Game/src/Shot.java
@@ -1,13 +1,36 @@
 import processing.core.PApplet;
+import processing.core.PVector;
 
 class Shot extends GameObject
 {
 	PApplet parent;
 	float theta;
-	float size;
+	float len;
 	float speed = 300;
-	float timeToLive;
-	float alive;
+	float lambda;
+	float on;
 	static int type;
+	
+	Shot(Game p, float x, float y, float theta, float len, float lambda, int type)
+	{
+	    parent = p;
+		pos = new PVector(x, y);
+		forward = new PVector(0, 1);
+		this.theta = theta;
+		this.len = len;
+		this.lambda = lambda;    
+		this.on = 0;
+		this.type = type;
+    }
+	
+	void render()
+	{
+		
+	}
+	
+	void update()
+	{
+		
+	}
 	
 }
