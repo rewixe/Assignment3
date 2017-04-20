66ce0d7467e98696d8982faf7f3c1b396bd2abf7
tnolan98 <c15406532@mydit.ie>
2017-04-20 13:38:03 +0100

    Change to vars

--- Game/src/Game.java
+++ Game/src/Game.java
@@ -1,10 +1,17 @@
 import java.util.ArrayList;
 
 import processing.core.PApplet;
+import processing.core.PFont;
 
 public class Game extends PApplet 
 {
 	public static Game applet;
+	
+	static int mode = 0;
+	//static int score;
+	PFont myFont;
+	PFont myFont2;
+	int lvlCnt = 1;
 	
 	static ArrayList<GameObject> gameObjects = new ArrayList<GameObject>();
 	static int mode = 0;
