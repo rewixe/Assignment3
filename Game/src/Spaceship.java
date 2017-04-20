2c9a5e796e5b88042fa203734ba8b23b6ec2ab8d
tnolan98 <c15406532@mydit.ie>
2017-04-20 14:01:48 +0100

    Constructor

--- Game/src/Spaceship.java
+++ Game/src/Spaceship.java
@@ -9,6 +9,7 @@ public class Spaceship extends GameObject
 	
 	PVector v;
 	PVector acc;
+	PVector f;
 	float theta; 
 	float radius;
     float wgt = 1;
@@ -24,6 +25,25 @@ public class Spaceship extends GameObject
 	
 	Spaceship(Game p, float x, float y, float theta, float size, char n, char s, char w, char e, char fire, int type)
 	{
+		parent = p;
+	    pos = new PVector(x, y);
+	    forward = new PVector(0, -1);
+	    acc = new PVector(0,0);
+	    v = new PVector(0,0);
+	    f = new PVector(0, 0);
+	    this.theta = theta;
+	    this.size = size;
+	    radius = size / 2;
+	    this.type = type;
+	    
+	    this.w = w;
+	    this.e = e;
+	    this.n = n;
+	    this.s = s;
+	    this.fire = fire;
+	    this.health = 10;
+	    Spaceship.ammo = 10;
+	    Spaceship.score = 80;
 		
 	}
 	
