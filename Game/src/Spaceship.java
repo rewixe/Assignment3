92ff266d420cf2f64489c161f9686acf45bbc05a
tnolan98 <c15406532@mydit.ie>
2017-04-20 13:48:14 +0100

    Methods

--- Game/src/Spaceship.java
+++ Game/src/Spaceship.java
@@ -1,6 +1,28 @@
 import processing.core.PApplet;
+import processing.core.PShape;
+import processing.core.PVector;
 
 public class Spaceship extends GameObject 
 {
 	
 	PApplet parent;
+	
+	PVector velocity;
+	PVector accel;
+	float theta; 
+	float radius;
+    float mass = 1;
+	PShape shape, flame;
+	char up, down, left, right;
+	
+	void render()
+	{
+		
+	}
+	
+	void update()
+	{
+		
+	}
+	
+}
